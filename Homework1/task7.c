0bffe846dd8b0c8f8d538f856794b2047307db76
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-09-22 02:42:52 +0300

    Completed task 7 from homework 1

---
+++ Homework1/task7.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int num)
+{
+    if(num == 1)
+    {
+        return 0;
+    }
+
+    int sqrtNum = sqrt(num);
+    
+    for(int i = 2; i <= round(sqrtNum); i++)
+    {
+        if(num % i == 0)
+        {
+            return 0;
+        }
+    }
+    
+    return 1;
+}
+
+int main()
+{
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if(number <= 1)
+    {
+        printf("There are no prime numbers that are less or equal to %d\n", number);
+        return 0;
+    }
+
+    printf("Prime numbers that are less or equal to %d: \n", number);
+    
+    for(int i = 1; i <= number; i++)
+    {
+        if(isPrime(i) == 1)
+        {
+            printf("%d\n", i);
+        }
+    }
+    
+    return 0;
+}
