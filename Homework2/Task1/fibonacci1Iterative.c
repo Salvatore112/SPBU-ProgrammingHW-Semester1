2b13af23b37de85eb20e6685ffcd69df0eaa40bf
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-10-07 21:27:28 +0300

    added tests

--- Homework2/Task1/fibonacci1Iterative.c
+++ Homework2/Task1/fibonacci1Iterative.c
@@ -1,34 +1,73 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-unsigned int collection[1000];
-
-unsigned int fibonacci(int number)
-{
-    if(number == 0)
-    {
-        collection[0] = 0;
-        return collection[0];
-    }
-    if(number == 1)
-    {
-        collection[1] = 1;
-        return collection[1];
-    }
-    collection[number] = collection[number - 1] + collection[number - 2];
-    return collection[number];
-}
+bool test();
+long long int fibonacci(int number);
 
 int main()
 {
-    int amount;
-    
-    printf("How many fibonacci collection do you want to see?: ");
-    scanf("%d", &amount);
-
-    for(int i = 0; i < amount; i++)
+    if (!test())
     {
-       printf("%u, ", fibonacci(i));
+        return 1;
     }
     
-    return 0;
+    int amount = 0;
+    
+    printf("How many fibonacci numbers do you want to see?: ");
+    scanf("%d", &amount);
+
+    for (int i = 0; i <= amount; i++)
+    {
+        printf("%lld, ", fibonacci(i));
+    }
+    
+}
+
+long long int fibonacci(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    
+    if (number == 1)
+    {
+        return 1;
+    }
+
+    long long int *collection = malloc(sizeof(long long int) * (number + 1));
+    if (collection == NULL)
+    {
+        printf("Out of memory!\n");
+        exit(1);
+    }
+    
+    collection[0] = 0;
+    collection[1] = 1;
+
+    for (int i = 2; i <= number; i++)
+    {
+        collection[i] = collection[i - 1] + collection[i - 2];
+    }
+    
+    return collection[number];
+
+    free(collection);
+}
+
+bool test()
+{
+    int FirstElementsSum = 0;
+    int expectedSum = 1569;
+    for (int i = 0; i <= 15; i++)
+    {
+        FirstElementsSum += fibonacci(i);
+    }
+    if (expectedSum - FirstElementsSum > 0.0000001)
+    {
+        printf("Test failed");
+        return false;
+    }
+    return true;
 }
