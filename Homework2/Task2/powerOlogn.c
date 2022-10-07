df97d340d21c95161d08d39fa6143866ee024d26
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-10-07 19:51:17 +0300

    added tests

--- Homework2/Task2/powerOlogn.c
+++ Homework2/Task2/powerOlogn.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 float power(int base, int exponent);
+bool tests();
 
 int main()
 {
+    if (!tests())
+    {
+        return 1;
+    } 
+    
     int base;
     int exponent;
 
@@ -12,11 +20,11 @@ int main()
     printf("Enter the base of the exponent: ");
     scanf("%d", &exponent);
 
-    if(exponent < 0)
+    if (exponent < 0)
     {
-        printf("%d^(%d) = %f\n", base, exponent, 1/power(base, -exponent));
+        printf("%d^(%d) = %f\n", base, exponent, 1 / power(base, -exponent));
     }
-    if(exponent >= 0)
+    if (exponent >= 0)
     {
         printf("%d^%d = %.0f\n", base, exponent, power(base, exponent));
     }
@@ -26,20 +34,58 @@ int main()
 
 float power(int base, int exponent)
 {
-    if(exponent == 0)
+    if (base == 0 && exponent == 0)
+    {
+        printf("0^0 is not defined! \n");
+        exit(1);
+    }
+    
+    if (exponent == 0)
     {
         return 1;
     }
     
-    if(exponent % 2 == 0 )
+    if (exponent % 2 == 0 )
     {
         float answer = power(base, exponent/2);
         return answer * answer;
     }
 
-    if(exponent % 2 != 0 )
+    if (exponent % 2 != 0 )
     {
         return base * power(base, exponent - 1);
     }
+}
 
+bool tests()
+{
+    float expectedAnswer1 = 1024;
+    if (power(2, 10) != expectedAnswer1)
+    {
+        printf("Failed when the exponent is positive\n");
+        return false;
+    }
+    
+    float expectedAnswer2 = 0.25;
+    if (1 / power(2, 2) - expectedAnswer2 > 0.0001)
+    {
+        printf("Failed when the exponent is negative\n");
+        return false;
+    } 
+ 
+    float expectedAnswer3 = 10;
+    if (power(10, 1) != expectedAnswer3)
+    {
+        printf("Failed when the exponent is 1\n");
+        return false;
+    } 
+
+    float expectedAnswer4 = 1;
+    if (power(10, 0) != expectedAnswer4)
+    {
+        printf("Failed when the exponent is 0\n");
+        return false;
+    } 
+
+    return true;
 }
