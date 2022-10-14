74e26d86a14df95b08b06b315fa618233a6825c3
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-10-15 01:58:34 +0300

    added functions definitions for the stack

---
+++ Homework6/stack.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "stack.h"
+
+typedef struct Node 
+{
+    int element;
+    struct Node *previous;
+} Node;
+
+void test()
+{
+    printf("ww");
+}
+
+int push(Node **head, int element)
+{
+    Node *temp = malloc(sizeof(Node));
+    if (temp == NULL)
+    {
+        printf("Out of memory");
+        return -1;
+    }
+    temp->element = element;
+    temp->previous = *head;
+    *head = temp;
+}
+
+int pop(Node **head, int *errorCode)
+{
+    if (*head == NULL)
+    {
+        *errorCode = -1;
+        return 0;
+    }
+    *errorCode = 0;
+    int value = (*head)->element;
+
+    Node *temp;
+    temp = (*head)->previous;
+    free(*head);
+    (*head) = temp;
+    return value;
+}
+
+bool isEmpty(Node *head)
+{
+    return head == NULL;
+} 
+
+int peek(Node *head)
+{
+    return head->element;
+}
+
+void clear(Node **head)
+{
+    while (!isEmpty(*head))
+    {
+        int errorCode = 0;
+        pop(head, &errorCode);
+        if (errorCode < 0)
+        {
+            break;
+        }
+    }
+}
