cf526ac57675145b0e7c4b702e94e073362b19c5
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-10-19 18:49:09 +0300

    fixed brackets

--- Homework1/task4.c
+++ Homework1/task4.c
@@ -2,23 +2,18 @@
 
 #define SIZE 28
 
-int main()
-{
+int main() {
     int arraySums[SIZE] = {0};
-    for(int i = 0; i < 10; i++)
-    {
-       for(int j = 0; j < 10; j++)
-       {
-            for(int k = 0; k < 10; k++)
-            {
+    for (int i = 0; i < 10; i++) {
+       for (int j = 0; j < 10; j++) {
+            for (int k = 0; k < 10; k++) {
                 arraySums[i + j + k] += 1;
             }
        } 
     }
 
     int count = 0;
-    for(int i = 0; i < SIZE; i++)
-    {
+    for (int i = 0; i < SIZE; i++) {
         count += arraySums[i] * arraySums[i];
     }
     printf("Total amount of lucky tickets: %d\n", count);
