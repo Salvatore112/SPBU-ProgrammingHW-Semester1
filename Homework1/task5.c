564da1a8a6d4dcbe5599df4f13ec7fa1a9c2b426
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-10-19 18:48:32 +0300

    fixed brackets

--- Homework1/task5.c
+++ Homework1/task5.c
@@ -4,10 +4,8 @@
 
 #define SIZE 1000
 
-bool closedBracket(char stack[], int *stackPosition, char openBracket)
-{
-    if (stack[*stackPosition] == openBracket && *stackPosition >= 0)
-    {
+bool closedBracket(char stack[], int *stackPosition, char openBracket) {
+    if (stack[*stackPosition] == openBracket && *stackPosition >= 0) {
         stack[*stackPosition] = '\0';
         *stackPosition -= 1;
         return true;
@@ -15,16 +13,13 @@ bool closedBracket(char stack[], int *stackPosition, char openBracket)
     return false;
 }
 
-bool isBalanced(char s[])
-{
+bool isBalanced(char s[]) {
 
     int stackPosition = -1;
     char stack[SIZE] = {'\0'};
     
-    for (int i = 0; i < strlen(s); i++)
-    {
-        switch (s[i])
-        {
+    for (int i = 0; i < strlen(s); i++) {
+        switch (s[i]) {
             case '(':
             case '{':
             case '[':
@@ -33,51 +28,43 @@ bool isBalanced(char s[])
             stack[stackPosition] = s[i];
             break;
             case ')':
-                if (!closedBracket(stack, &stackPosition, '('))
-                {
+                if (!closedBracket(stack, &stackPosition, '(')) {
                     return false;
                 }
                 break;
             case ']':
-                if (!closedBracket(stack, &stackPosition, '['))
-                {
+                if (!closedBracket(stack, &stackPosition, '[')) {
                     return false;
                 }
                 break;
             case '}':
-                if (!closedBracket(stack, &stackPosition, '{'))
-                {
+                if (!closedBracket(stack, &stackPosition, '{')) {
                     return false;
                 }
                 break;
             case '>':
-                if (!closedBracket(stack, &stackPosition, '<'))
-                {
+                if (!closedBracket(stack, &stackPosition, '<')) {
                     return false;
                 }
                 break;
         }
     }
-    if (stackPosition == -1)
-    {
+    if (stackPosition == -1) {
         return true;
     }
     return false;
 }
 
-int main()
-{
+int main() {
     char string[SIZE];
     
     printf("Please enter a string(less or equal to 1000 characters): ");
     scanf("%s", string);
     
-    if (isBalanced(string))
-    {
+    if (isBalanced(string)) {
         printf("The sequence is balanced\n");
     }
-    else
-    {
+    else {
         printf("The sequence isn't balanced\n");
     }
     return 0;
