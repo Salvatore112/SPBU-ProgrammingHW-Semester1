7f12a0ccfa437bd8c2b595e9f0b5d0989af3d296
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-10-20 22:22:40 +0300

    fixed brackets

--- Homework2/Task3/bubbleSort3.c
+++ Homework2/Task3/bubbleSort3.c
@@ -8,10 +8,8 @@ void bubbleSort(int array[], int length);
 bool testsSorts();
 bool checkSorted(int array[], int arrayLength);
 
-int main()
-{
-    if (!testsSorts())
-    {
+int main() {
+    if (!testsSorts()) {
         return 1;
     }
 
@@ -19,8 +17,7 @@ int main()
     
     printf ("Enter the length of the array: ");
     scanf ("%d", &length);
-    while (length <= 0)
-    {
+    while (length <= 0) {
         printf("Enter a natural number!\n");
         printf ("Enter the length of the array: ");
         scanf ("%d", &length);
@@ -28,14 +25,12 @@ int main()
 
     int *array = malloc(length * sizeof (int));
 
-    if(array == NULL)
-    {
+    if (array == NULL) {
         printf("Memory is not available!\n");
         return 1;
     }
 
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
         printf ("Enter the element #%d: ", i);
         scanf ("%d: ", &array[i]);
     }
@@ -43,8 +38,7 @@ int main()
     bubbleSort(array, length);
 
     printf("The sorted array: ");
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
         printf ("%d ", array[i]);
     }
 
@@ -52,20 +46,16 @@ int main()
     return 0;
 }
 
-void bubbleSort(int array[], int length)
-{
+void bubbleSort(int array[], int length) {
     int temp;
     int i = 0;
     bool swapped = false;
     
-    do
-    {
+    do {
         swapped = false;
         
-        for (int j = 0; j < length - 1 - i; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
+        for (int j = 0; j < length - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
@@ -78,40 +68,33 @@ void bubbleSort(int array[], int length)
     } while (swapped);
 }
 
-bool testsSorts()
-{
+bool testsSorts() {
     int testArray1[lengthT] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     bubbleSort(testArray1, lengthT);
-    if (!checkSorted(testArray1, lengthT))
-    {   
+    if (!checkSorted(testArray1, lengthT)) {   
         printf ("Sort Failed when the elements are the same\n");
         return false;
     }
     
     int testArray2[lengthT] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     bubbleSort(testArray2, lengthT);
-    if (!checkSorted(testArray2, lengthT))
-    {   
+    if (!checkSorted(testArray2, lengthT)) {   
         printf ("Sort Failed when the array is sorted\n");
         return false;
     }
 
     int testArray3[lengthT] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
     bubbleSort(testArray3, lengthT);
-    if (!checkSorted(testArray3, lengthT))
-    {   
+    if (!checkSorted(testArray3, lengthT)) {   
         printf ("Sort Failed when the array is reversed\n");
         return false;
     }
     return true;
 }
 
-bool checkSorted(int array[], int arrayLength)
-{   
-    for (int i = 0; i < arrayLength - 1; i++)
-    {
-        if (array[i] > array[i + 1])
-        {
+bool checkSorted(int array[], int arrayLength) {   
+    for (int i = 0; i < arrayLength - 1; i++) {
+        if (array[i] > array[i + 1]) {
             return false;
         }
     }
