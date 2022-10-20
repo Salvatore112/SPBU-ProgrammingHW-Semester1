a3a1f5b4306557cce22079b7f327cfc086a5e767
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-10-20 22:27:19 +0300

    fixed brackets

--- Homework3/task1.c
+++ Homework3/task1.c
@@ -12,16 +12,14 @@ int quickSort(int array[], int low, int high);
 bool tests();
 bool checkSorted();
 
-int main()
-{
-    if(tests()){
+int main() {
+    if (tests()) {
         int size;
 
         printf("Enter the size of the array: ");
         scanf("%d", &size);
 
-        while(size <= 0)
-        {
+        while (size <= 0) {
             printf("Invalid input! (size must be greater than 0)\n");
             printf("Enter the size of the array: ");
             scanf("%d", &size);
@@ -29,8 +27,7 @@ int main()
         
         int *array = malloc(size * sizeof(int));
 
-        for(int i = 0; i < size; i++)
-        {
+        for (int i = 0; i < size; i++) {
             printf("Enter the element #%d: ", i);
             scanf("%d", &array[i]);
         }
@@ -47,72 +44,58 @@ int main()
     }
 }
 
-bool tests()
-{
+bool tests() {
     
     int testArray2[length] = {1, 5, 8, 3, 7, 9, 2, 3, 4, 100};
     quickSort(testArray2, 0, 9);
-    if(!checkSorted(testArray2, length))
-    {   
+    if (!checkSorted(testArray2, length)) {   
         printf("Failed when the pivot is the biggest element");
         return false;
     }
 
     int testArray3[length] = {122, 5, 8, 3, 7, 9, 2, 3, 4, 1};
     quickSort(testArray3, 0, 9);
-    if(!checkSorted(testArray3, length))
-    {   
+    if (!checkSorted(testArray3, length)) {   
         printf("Failed when the pivot is the smallest element");
         return false;
     }
     
     int testArray1[length] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     quickSort(testArray1, 0, 9);
-    if(!checkSorted(testArray1, length))
-    {   
+    if (!checkSorted(testArray1, length)) {   
         printf("Failed when the elements are the same");
         return false;
     }
     
     int testArray[length] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     quickSort(testArray, 0, 9);
-    if(!checkSorted(testArray, length))
-    {   
+    if (!checkSorted(testArray, length)) {   
         printf("Failed when the array is sorted");
         return false;
     }
     return true;
-
 }
 
-bool checkSorted(int array[], int arrayLength)
-{   
-    for(int i = 0; i < arrayLength - 1; i++)
-    {
-        if(array[i] > array[i + 1])
-        {
+bool checkSorted(int array[], int arrayLength) {   
+    for (int i = 0; i < arrayLength - 1; i++) {
+        if (array[i] > array[i + 1]) {
             return false;
         }
     }
     return true;
 }
 
-void display(int array[], int size)
-{
-    for(int i = 0; i < size; i++)
-    {
+void display(int array[], int size) {
+    for (int i = 0; i < size; i++) {
         printf("%d ", array[i]);
     }
 }
 
-void insertionSort(int array[], int low, int high)
-{
-    for(int i = low; i < high; i++)
-    {
+void insertionSort(int array[], int low, int high) {
+    for (int i = low; i < high; i++) {
         int key = array[i];
         int j = i - 1;
-        while(j >= 0 && array[j] >= key)
-        {
+        while (j >= 0 && array[j] >= key) {
             array[j + 1] = array[j];
             j--;
         }
@@ -120,23 +103,19 @@ void insertionSort(int array[], int low, int high)
     }
 }
 
-void swap(int *firstVal, int *secondVal)
-{
+void swap(int *firstVal, int *secondVal) {
     int temp = *firstVal;
     *firstVal = *secondVal;
     *secondVal = temp;
 }
 
-int partition(int array[], int low, int high)
-{
+int partition(int array[], int low, int high) {
     int pivotVal = array[high];
     
     int i = low;
     
-    for(int j = low; j < high; j++)
-    {
-        if(array[j] <= pivotVal)
-        {
+    for (int j = low; j < high; j++) {
+        if (array[j] <= pivotVal) {
             swap(&array[j], &array[i]);
             i++;
         }
@@ -145,31 +124,24 @@ int partition(int array[], int low, int high)
     return i;
 }
 
-int quickSort(int array[], int low, int high)
-{
-    if(high - low + 1 < 10)
-    {
+int quickSort(int array[], int low, int high) {
+    if (high - low + 1 < 10) {
         insertionSort(array, low, high + 1);
         return 0;
     }
     
-    if(low < high)
-    {
+    if (low < high) {
         int pivotIndex = partition(array, 0, high);
-        if(pivotIndex + 1 < 10)
-        {
+        if (pivotIndex + 1 < 10) {
             insertionSort(array, 0, pivotIndex + 1);
         }
-        else
-        {
+        else {
             quickSort(array, 0, pivotIndex - 1); 
         }
-        if(high - pivotIndex + 1 < 10)
-        {
+        if (high - pivotIndex + 1 < 10) {
             insertionSort(array, pivotIndex + 1, high + 1);
         }
-        else
-        {
+        else {
             quickSort(array, pivotIndex + 1, high);
         }
     }
