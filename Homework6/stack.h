4681b3700b749005178248e034fca871c8518430
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-10-20 23:23:24 +0300

    updated stack.h for task2

--- Homework6/stack.h
+++ Homework6/stack.h
@@ -6,27 +6,25 @@
 //struct that holds the element at the top and the pointer to the element under it
 typedef struct Node 
 {
-    char* element;
+    char element;
     struct Node *previous;
 } Node; 
 
 //Function that adds a new element at the top of the stack
-char* push(Node **head, char *element); 
+char push(Node **head, char element); 
 
 //Function that removes a new element at the top of the stack
-char* pop(Node **head, int *errorCode); 
+char pop(Node **head, int *errorCode); 
 
 //Function that checks if the stack is empty
 bool isEmpty(Node *head); 
 
 //Function that returns the current element at the top of the stack
-char* peek(Node *head); 
+char peek(Node *head); 
 
 //Function that removes all the elements from the stack
 void clear(Node **head); 
 
-//Function that displays all the elements of the stack
-void displayStack(Node *stack); 
 
 
 
