5ef6ae5354573119985088687cb845c80e30e6c6
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-11-10 00:40:06 +0300

    fixed small inaccuracies

--- Homework1/task2.c
+++ Homework1/task2.c
@@ -1,70 +1,64 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int partialDevident(int devident, int deviser);
+int partialQuotient(int dividend, int divisor);
 
 int main() {
-    int devident = 0;
-    int deviser = 0;
+    int dividend = 0;
+    int divisor = 0;
     
-    printf("Please enter a devident: ");
-    scanf("%d", &devident);
-    printf("Please enter a deviser: ");
-    scanf("%d", &deviser);
+    printf("Please enter a dividend: ");
+    scanf("%d", &dividend);
+    printf("Please enter a divisor: ");
+    scanf("%d", &divisor);
     
-    printf("Incomplete quotient = %d\n", partialDevident(devident, deviser));
+    printf("Incomplete quotient = %d\n", partialQuotient(dividend, divisor));
 }
 
-int partialDevident(int devident, int deviser) {
-    bool devidentIsNegative = false; 
-    bool deviserIsNegative = false; 
+int partialQuotient(int dividend, int divisor) {
+    bool dividendIsNegative = false; 
+    bool divisorIsNegative = false; 
     
-    if (devident < 0) {
-        devident = -devident;
-        devidentIsNegative = true;
+    if (dividend < 0) {
+        dividend = -dividend;
+        dividendIsNegative = true;
     }
-    if (deviser < 0) {
-        deviser = -deviser; 
-        deviserIsNegative = true;
+    if (divisor < 0) {
+        divisor = -divisor; 
+        divisorIsNegative = true;
     }
 
-    int initialDevident = devident;
-    int initialDeviser = deviser;
+    int initialdividend = dividend;
+    int initialdivisor = divisor;
     int quotient = 0;
     int count = 0;
 
-    while (devident > deviser) {
-        devident -= deviser;
+    while (dividend > divisor) {
+        dividend -= divisor;
     }
         
-    quotient = devident; 
-    initialDevident -= quotient;
+    quotient = dividend; 
+    initialdividend -= quotient;
 
-    while (initialDevident != 0) {
-        initialDevident -= deviser;
+    while (initialdividend != 0) {
+        initialdividend -= divisor;
         count++;
     }
 
-    bool answerNegative = true;
-
-    if (!devidentIsNegative && !deviserIsNegative || devidentIsNegative && deviserIsNegative) {
-        answerNegative = false;
-    }
+    bool answerNegative = dividendIsNegative != divisorIsNegative;
     
     if (answerNegative) {
-       if (devidentIsNegative && !deviserIsNegative) {
+       if (dividendIsNegative && !divisorIsNegative) {
             return -count - 1;
-       }
-       else {
+        } else {
             return -count;
-       }
+        }
     } 
     
     if (!answerNegative) {
-       if (devidentIsNegative && deviserIsNegative) {
+       if (dividendIsNegative && divisorIsNegative) {
             return count + 1;
-       }
-       else {
+       } else {
             return count;
        }
     } 
