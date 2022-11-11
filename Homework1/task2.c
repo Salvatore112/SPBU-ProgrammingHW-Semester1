e95d5bf515bcfe783a5e40615c9a088ccc8e9302
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-11-11 14:30:43 +0300

    fixed

--- Homework1/task2.c
+++ Homework1/task2.c
@@ -6,60 +6,41 @@ int partialQuotient(int dividend, int divisor);
 int main() {
     int dividend = 0;
     int divisor = 0;
-    
+
     printf("Please enter a dividend: ");
     scanf("%d", &dividend);
     printf("Please enter a divisor: ");
     scanf("%d", &divisor);
-    
+
     printf("Incomplete quotient = %d\n", partialQuotient(dividend, divisor));
 }
 
 int partialQuotient(int dividend, int divisor) {
-    bool dividendIsNegative = false; 
-    bool divisorIsNegative = false; 
-    
+    bool dividendIsNegative = false;
+    bool divisorIsNegative = false;
+
     if (dividend < 0) {
         dividend = -dividend;
         dividendIsNegative = true;
     }
     if (divisor < 0) {
-        divisor = -divisor; 
+        divisor = -divisor;
         divisorIsNegative = true;
     }
 
-    int initialdividend = dividend;
-    int initialdivisor = divisor;
-    int quotient = 0;
+    int initialDividend = dividend;
+
     int count = 0;
-
-    while (dividend > divisor) {
-        dividend -= divisor;
-    }
-        
-    quotient = dividend; 
-    initialdividend -= quotient;
-
-    while (initialdividend != 0) {
-        initialdividend -= divisor;
+    while (initialDividend > divisor) {
+        initialDividend -= divisor;
         count++;
     }
 
     bool answerNegative = dividendIsNegative != divisorIsNegative;
-    
+
     if (answerNegative) {
-       if (dividendIsNegative && !divisorIsNegative) {
-            return -count - 1;
-        } else {
-            return -count;
-        }
-    } 
-    
-    if (!answerNegative) {
-       if (dividendIsNegative && divisorIsNegative) {
-            return count + 1;
-       } else {
-            return count;
-       }
-    } 
+        return (dividendIsNegative && !divisorIsNegative) ? -count - 1 : -count;
+    }
+
+    return (dividendIsNegative && divisorIsNegative) ? count + 1 : count;
 }
