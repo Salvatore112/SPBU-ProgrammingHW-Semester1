86b2a4339249615a6cbb2626eb566b2e0a2d2db5
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-11-18 15:31:20 +0300

    fixed iterative power

--- Homework2/Task2/powerOn.c
+++ Homework2/Task2/powerOn.c
@@ -2,74 +2,87 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-float power(int base, int exponent);
+float power(int base, int exponent, int *errorCode);
 bool tests();
 
 int main() {
     if (!tests()) {
         return 1;
     }
-    
+
     int base = 0;
     int exponent = 0;
+    int errorCode = 0;
 
     printf("Enter the base of the power: ");
     scanf("%d", &base);
     printf("Enter the base of the exponent: ");
     scanf("%d", &exponent);
 
-    if (exponent < 0) {
-        printf("%d^(%d) = %f\n", base, exponent, 1/power(base, -exponent));
-    }
-    if (exponent >= 0) {
-        printf("%d^%d = %.0f\n", base, exponent, power(base, exponent));
+    float answer = power(base, exponent, &errorCode);
+    if (errorCode != 0) {
+        printf("Not defined");
+    } else {
+        printf("%.5f", answer);
     }
     
     return 0;
 }
 
-float power(int base, int exponent) {
-    if (base == 0 && exponent == 0) {
-        printf("0^0 is not defined! \n");
-        exit(1);
+float power(int base, int exponent, int *errorCode) {
+    bool wasNegative = true;
+    if (exponent > 0) {
+        wasNegative = false;
     }
-    
+    exponent = abs(exponent);
+
+    if (base == 0 && exponent == 0) {
+        *errorCode = 1;
+        return 1;
+    }
+
     float answer = 1;
-    
+
     for (int i = 0; i < exponent; i++) {
         answer = answer * base;
     }
-    
-    return answer;
+
+    *errorCode = 0;
+    return wasNegative ? 1.0f / answer : answer;
 }
 
 bool tests() {
     float expectedAnswer1 = 1024;
-    if (power(2, 10) != expectedAnswer1) {
+    int errorCode1 = 1;
+    if (power(2, 10, &errorCode1) != expectedAnswer1) {
         printf("Failed when the exponent is positive\n");
         return false;
     }
-    
+
     float expectedAnswer2 = 0.25;
-    if (1 / power(2, 2) - expectedAnswer2 > 0.0001) {
+    int errorCode2 = 1;
+    if (power(2, -4, &errorCode2) - expectedAnswer2 > 0.0001) {
         printf("Failed when the exponent is negative\n");
         return false;
-    } 
- 
+    }
+
     float expectedAnswer3 = 10;
-    if (power(10, 1) != expectedAnswer3) {
+    int errorCode3 = 1;
+    if (power(10, 1, &errorCode3) != expectedAnswer3) {
         printf("Failed when the exponent is 1\n");
         return false;
-    } 
+    }
 
     float expectedAnswer4 = 1;
-    if (power(10, 0) != expectedAnswer4) {
+    int errorCode4 = 1;
+    if (power(10, 0, &errorCode4) != expectedAnswer4) {
         printf("Failed when the exponent is 0\n");
         return false;
-    } 
+    }
 
     return true;
 }
+
 
 
 
