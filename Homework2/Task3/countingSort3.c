a52b330e69c25ff646280092d6d51782768760df
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-11-25 20:02:55 +0300

    style guide fixes, compilation optimization fixes

--- Homework2/Task3/countingSort3.c
+++ Homework2/Task3/countingSort3.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <limits.h>
 
-#define MIN -100
-#define lengthT 10
+#define TEST_LENGTH 10
 
-void countingSort(int array[], int size);
-bool checkSorted(int array[], int arrayLength);
+int countingSort(int array[], int size);
+bool checkSorted(const int array[], int arrayLength);
 bool testsSorts();
 
 int main() {
     if (!testsSorts()) {
         return 1;
     }
-    
+
     int length = 0;
     printf("Enter the length of the array: ");
     scanf("%d", &length);
-    
+
     int *array = malloc(length * sizeof(int));
     if (array == NULL) {
         printf("Memory is not available\n");
@@ -29,19 +29,21 @@ int main() {
         scanf("%d", &array[i]);
     }
 
-    countingSort(array, length);
-    
+    if (countingSort(array, length) == 1) {
+        printf("Out of memory\n");
+        return 1;
+    }
+
     printf("The sorted array: \n");
     for (int i = 0; i < length; i++) {
         printf("%d ", array[i]);
-    }  
+    }
 
     return 0;
 }
 
-void countingSort(int array[], int size)
-{
-    int maxElement = MIN;
+int countingSort(int array[], int size) {
+    int maxElement = array[0];
     for (int i = 0; i < size; i++) {
         if (array[i] > maxElement) {
             maxElement = array[i];
@@ -50,33 +52,28 @@ void countingSort(int array[], int size)
 
     maxElement++;
 
-    int *indexList = malloc(maxElement * sizeof(int));
+    int *indexList = calloc(maxElement, sizeof(int));
     if (indexList == NULL) {
-        printf("Memory is not available");
-        exit(1);
+        return 1;
     }
 
-    for (int j = 0; j < maxElement; j++) {
-        indexList[j] = 0;
-    }
-    
     for (int j = 0; j < size; j++) {
         indexList[array[j]]++;
     }
 
     for (int j = 1; j < maxElement; j++) {
-        indexList[j]+=indexList[j - 1];
+        indexList[j] += indexList[j - 1];
     }
-    
+
     for (int j = maxElement; j > 0; j--) {
-        indexList[j]=indexList[j - 1];
+        indexList[j] = indexList[j - 1];
     }
     indexList[0] = 0;
 
     int *outputArray = malloc(size * sizeof(int));
-    if(outputArray == NULL) {
-        printf("Memory is not available");
-        exit(1);
+    if (outputArray == NULL) {
+        free(indexList);
+        return 1;
     }
 
     for (int i = 0; i < size; i++) {
@@ -86,37 +83,38 @@ void countingSort(int array[], int size)
 
     for (int i = 0; i < size; i++) {
         array[i] = outputArray[i];
-    } 
+    }
 
     free(indexList);
     free(outputArray);
-} 
+    return 0;
+}
 
 bool testsSorts() {
-    int testArray1[lengthT] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    countingSort(testArray1, lengthT);
-    if (!checkSorted(testArray1, lengthT)) {   
+    int testArray1[TEST_LENGTH] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    countingSort(testArray1, TEST_LENGTH);
+    if (!checkSorted(testArray1, TEST_LENGTH)) {
         printf ("Sort Failed when the elements are the same\n");
         return false;
     }
-    
-    int testArray2[lengthT] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    countingSort(testArray2, lengthT);
-    if (!checkSorted(testArray2, lengthT)) {   
+
+    int testArray2[TEST_LENGTH] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    countingSort(testArray2, TEST_LENGTH);
+    if (!checkSorted(testArray2, TEST_LENGTH)) {
         printf ("Sort Failed when the array is sorted\n");
         return false;
     }
 
-    int testArray3[lengthT] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    countingSort(testArray3, lengthT);
-    if (!checkSorted(testArray3, lengthT)) {   
+    int testArray3[TEST_LENGTH] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    countingSort(testArray3, TEST_LENGTH);
+    if (!checkSorted(testArray3, TEST_LENGTH)) {
         printf ("Sort Failed when the array is reversed\n");
         return false;
     }
     return true;
 }
 
-bool checkSorted(int array[], int arrayLength) {   
+bool checkSorted(const int array[], int arrayLength) {
     for (int i = 0; i < arrayLength - 1; i++) {
         if (array[i] > array[i + 1]) {
             return false;
