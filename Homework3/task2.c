fb296242e91bbe11b8c9274efa1464ca573fd83f
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-11-25 21:02:03 +0300

    fixed spaces

--- Homework3/task2.c
+++ Homework3/task2.c
@@ -28,36 +28,37 @@ int main() {
         printf("Enter the length of the array: ");
         scanf("%d", &length);
     }
-    
+
     printf("How many numbers do you want to check if they're in the array?: ");
     scanf("%d", &amount);
     while (amount <= 0) {
         printf("Invalid input you must enter a natural number.\n");
         printf("Enter the length of the array: ");
         scanf("%d", &amount);
-    } 
-    
+    }
+
     int *array = malloc(length * sizeof(int));
     if (array == NULL) {
         printf("Memory is not available!\n");
-        exit(1);
+        return 1;
     }
 
     int *numbers = malloc(amount * sizeof(int));
     if (numbers == NULL) {
         printf("Memory is not available!\n");
-        exit(1);
+        free(array);
+        return 1;
     }
-    
+
     for (int i = 0; i < length; i++) {
         array[i] = rand() % 101;
     }
     for (int i = 0; i < amount; i++) {
         numbers[i] = rand() % 101;
     }
-    
+
     quickSort(array, 0, length - 1);
-    
+
     printf("Generated array: ");
     for (int i = 0; i < length; i++) {
         printf("%d ", array[i]);
@@ -68,9 +69,9 @@ int main() {
         printf("%d ", numbers[i]);
     }
     printf("\n");
-    
+
     int count = 0;
-    
+
     for (int j = 0; j < amount; j++) {
         if (binarySearch(array, numbers[j], 0, length - 1) > 0) {
             printf("The number %d is in the array! \n", numbers[j]);
@@ -81,7 +82,7 @@ int main() {
     if (count == 0) {
         printf("none of these numbers are in the array \n");
     }
-    
+
     free(numbers);
     free(array);
     return 0;
@@ -91,18 +92,14 @@ int binarySearch(int array[], int element, int left, int right) {
     if (left > right) {
         return -1;
     }
-    
-    int mid = left + (right - left)/2;
-    
+
+    int mid = (left + right) / 2;
+
     if (array[mid] == element) {
         return mid;
-    }
-    
-    else if (array[mid] > element) {
+    } else if (array[mid] > element) {
         return binarySearch(array, element, left, mid - 1);
-    }
-    
-    else {
+    } else {
         return binarySearch(array, element, mid + 1, right);
     }
 }
@@ -115,10 +112,10 @@ void swap(int *firstNumber, int *secondNumber) {
 
 int partition(int array[], int low, int high) {
     int pivotVal = array[high];
-    
+
     int i = low;
-                                        
-    for (int j = low; j < high; j++) {                                   
+
+    for (int j = low; j < high; j++) {
         if (array[j] <= pivotVal) {
             swap(&array[j], &array[i]);
             i++;
@@ -139,28 +136,28 @@ void quickSort(int array[], int low, int high) {
 bool testsSorts() {
     int testArray2[lengthT] = {1, 5, 8, 3, 7, 9, 2, 3, 4, 100};
     quickSort(testArray2, 0, 9);
-    if (!checkSorted(testArray2, lengthT)) {   
+    if (!checkSorted(testArray2, lengthT)) {
         printf("Sort Failed when the pivot is the biggest element");
         return false;
     }
 
     int testArray3[lengthT] = {122, 5, 8, 3, 7, 9, 2, 3, 4, 1};
     quickSort(testArray3, 0, 9);
-    if (!checkSorted(testArray3, lengthT)) {   
+    if (!checkSorted(testArray3, lengthT)) {
         printf("Sort Failed when the pivot is the smallest element");
         return false;
     }
-    
+
     int testArray1[lengthT] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     quickSort(testArray1, 0, 9);
-    if (!checkSorted(testArray1, lengthT)) {   
+    if (!checkSorted(testArray1, lengthT)) {
         printf("Sort Failed when the elements are the same");
         return false;
     }
-    
+
     int testArray[lengthT] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     quickSort(testArray, 0, 9);
-    if (!checkSorted(testArray, lengthT)) {   
+    if (!checkSorted(testArray, lengthT)) {
         printf("Sort Failed when the array is sorted");
         return false;
     }
@@ -188,7 +185,7 @@ bool testsSearch() {
     return true;
 }
 
-bool checkSorted(int array[], int arrayLength) {   
+bool checkSorted(int array[], int arrayLength) {
     for (int i = 0; i < arrayLength - 1; i++) {
         if (array[i] > array[i + 1]) {
             return false;
