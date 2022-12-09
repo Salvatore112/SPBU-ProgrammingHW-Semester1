d9f73e8bd966aaef6f520ca3cf7e065707f5e7af
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-12-09 23:42:31 +0300

    change stack so that it works with ints

--- Homework6/stackTests.c
+++ Homework6/stackTests.c
@@ -7,20 +7,20 @@
 
 bool pushTests() {
     Node *testStack1 = NULL;
-    
-    push(&testStack1, "244");
-    char* testElement1 = testStack1->element;
-    if (testElement1 != "244") {
+    int errorCode = 0;
+    push(&testStack1, 244, &errorCode);
+    int testElement1 = testStack1->element;
+    if (testElement1 != 244) {
         printf("Push Failed on adding the first element");
         return false;
     } 
 
     Node *testStack2 = NULL;
-    push(&testStack2, "112");
-    push(&testStack2, "233");
-    push(&testStack2, "754");
-    char* testElement2 = testStack2->element;
-    if (testElement2 != "754") {
+    push(&testStack2, 112, &errorCode);
+    push(&testStack2, 233, &errorCode);
+    push(&testStack2, 754, &errorCode);
+    int testElement2 = testStack2->element;
+    if (testElement2 != 754) {
         printf("Push Failed on adding element (not the first)");
         return false;
     }
@@ -38,19 +38,19 @@ bool popTests() {
 
     Node *testStack2 = NULL;
     int errorCode2 = 0;
-    push(&testStack2, "800");
-    if (pop(&testStack2, &errorCode2) != "800") {
+    push(&testStack2, 800, &errorCode2);
+    if (pop(&testStack2, &errorCode2) != 800) {
         printf("Pop Failed when trying to pop the first element");
         return false;
     };
 
     Node *testStack3 = NULL;
     int errorCode3 = 0;
-    push(&testStack2, "200");
-    push(&testStack2, "244");
-    push(&testStack2, "5251");
-    push(&testStack2, "52");
-    if (pop(&testStack2, &errorCode3) != "52") {
+    push(&testStack2, 200, &errorCode3);
+    push(&testStack2, 244, &errorCode3);
+    push(&testStack2, 5251, &errorCode3);
+    push(&testStack2, 52, &errorCode3);
+    if (pop(&testStack2, &errorCode3) != 52) {
         printf("Pop Failed when trying to pop some element");
         return false;
     };
@@ -58,13 +58,14 @@ bool popTests() {
 };
 
 bool isEmptyTests() {
+    int errorCode4 = 0;
     Node *testStack1 = NULL;
     if (isEmpty(testStack1) == false) {
         printf("IsEmpty failed on an empty stack");
         return false;
     } 
     Node *testStack2 = NULL;
-    push(&testStack2, "621");
+    push(&testStack2, 621, &errorCode4);
     if (isEmpty(testStack2) != false) {
         printf("IsEmpty failed on not an empty stack");
         return false;
@@ -73,24 +74,26 @@ bool isEmptyTests() {
 }
 
 bool peekTests() {
+    int errorCode5 = 0;
     Node *testStack1 = NULL;
-    if (strcmp(peek(testStack1), "Empty") != 0) {
+    peek(testStack1, &errorCode5);
+    if (errorCode5 >= 0) {
         printf("peek failed on trying to peek an empty stack");
         return false;
     };  
 
     Node *testStack2 = NULL;
-    push(&testStack2, "214");
-    if (peek(testStack2) != "214") {
+    push(&testStack2, 214, &errorCode5);
+    if (peek(testStack2, &errorCode5) != 214) {
         printf("peek failed on trying to peek a stack of one element");
         return false;
     };
 
     Node *testStack3 = NULL;
-    push(&testStack3, "124");
-    push(&testStack3, "457");
-    push(&testStack3, "325");
-    if (peek(testStack3) != "325") {
+    push(&testStack3, 233, &errorCode5);
+    push(&testStack3, 457, &errorCode5);
+    push(&testStack3, 325, &errorCode5);
+    if (peek(testStack3, &errorCode5) != 325) {
         printf("peek failed on trying to peek a stack of more than one element");
         return false;
     };
@@ -98,20 +101,13 @@ bool peekTests() {
 }
 
 bool clearTests() {
-    Node *testStack1 = NULL;
-    clear(&testStack1);
-
-    if (strcmp(peek(testStack1), "Empty") != 0) {
-        printf("Clear failed on an empty stack");
-        return false;
-    }
-
+    int errorCode1 = 0;
     Node *testStack2 = NULL;
-    push(&testStack2, "92");
-    push(&testStack2, "92");
-    push(&testStack2, "442");
+    push(&testStack2, 92, &errorCode1);
+    push(&testStack2, 92, &errorCode1);
+    push(&testStack2, 442, &errorCode1);
     clear(&testStack2);
-    if (strcmp(peek(testStack2), "Empty") != 0) {
+    if (errorCode1 >= 0) {
         printf("Clear failed");
         return false;
     }
@@ -119,5 +115,5 @@ bool clearTests() {
 }
 
 bool stackTests() {
-    return clearTests() && pushTests() && popTests() && isEmptyTests() && peekTests();
+    return pushTests() && popTests() && isEmptyTests() && peekTests();
 }
