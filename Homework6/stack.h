ffb94f975b608cc0991024863bc849aff52fd29d
Pavel Averin <113641510+Salvatore112@users.noreply.github.com>
2022-12-09 23:42:08 +0300

    change stack so that it has int field

--- Homework6/stack.h
+++ Homework6/stack.h
@@ -4,28 +4,27 @@
 #include <stdbool.h>
 
 //struct that holds the element at the top and the pointer to the element under it
-typedef struct Node 
-{
-    char* element;
+typedef struct Node {
+    int element;
     struct Node *previous;
 } Node; 
 
-//Function that adds a new element at the top of the stack
-char* push(Node **head, char *element); 
+// Function that adds a new element at the top of the stack.
+void push(Node **head, int element, int *errorCode);
 
-//Function that removes a new element at the top of the stack
-char* pop(Node **head, int *errorCode); 
+// Function that removes a new element at the top of the stack.
+int pop(Node **head, int *errorCode);
 
-//Function that checks if the stack is empty
-bool isEmpty(Node *head); 
+// Function that checks if the stack is empty.
+bool isEmpty(Node *head);
 
-//Function that returns the current element at the top of the stack
-char* peek(Node *head); 
+// Function that returns the current element at the top of the stack.
+int peek(Node *head, int *errorCode);
 
-//Function that removes all the elements from the stack
+// Function that removes all the elements from the stack.
 void clear(Node **head); 
 
-//Function that displays all the elements of the stack
+// Function that displays all the elements of the stack.
 void displayStack(Node *stack); 
 
 
